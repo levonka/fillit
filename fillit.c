6f062542e2de7e73c11a58a2f6d26809264e98ff
Adeline Gottlieb <agottlie@e1r8p3.21-school.ru>
2018-12-19 18:11:44 +0300

    ft_create_ttr

--- fillit.c
+++ fillit.c
@@ -6,41 +6,60 @@
 /*   By: agottlie <agottlie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 16:07:22 by agottlie          #+#    #+#             */
-/*   Updated: 2018/12/19 16:26:54 by agottlie         ###   ########.fr       */
+/*   Updated: 2018/12/19 18:09:51 by agottlie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-typedef struct		s_base_ttr
+t_ttr	*ft_create_ttr(char *template)
 {
-	unsigned char	height;
-	unsigned char	width;
-	char			*template;
-	unsigned char	quantity;
-	s_base_ttr		*next;
-}					t_base_ttr;
+	t_ttr	*new;
 
-typedef struct		s_ttr
+	new = (t_ttr *)malloc(sizeof(t_ttr));
+	ISMALLOC_LL(new);
+	if (template == NULL)
+		return (NULL);
+
+	new->template = ft_strdup(template);
+	ISMALLOC_LL(new->template);
+	new->next = NULL;
+	return (new);
+}
+
+t_base_ttr	*ft_template_ll(char *templates)
 {
-	unsigned char	height;
-	unsigned char	width;
-	char			*template;
-	unsigned char	letter;
-	s_ttr			*next;
-}					t_ttr;
+	t_base_ttr		*new;
+	unsigned char	i;
 
-t_ttr	ft_create_ttr()
-{
-	t_ttr	*new_struct;
+	i = 0;
+	new = (t_base_ttr *)malloc(sizeof(t_base_ttr));
+	ISMALLOC_LL(new);
+	if (templates == NULL)
+		return (NULL);
 
-	new_struct = (t_ttr)malloc(sizeof(t_ttr));
-	ISMALLOC(new_struct);
+	while (templates[i] != '\0')
+	{
+		/*	НАПИСАТЬ ФУНКЦИЮ, КОТОРАЯ БУДЕТ СОЗДАВАТЬ ЛИСТ
+		ИЗ 19 СТРУКТУР, В КАЖДОЙ ИЗ КОТОРЫХ БУДЕТ TEMPLATE,
+		ВЫСОТА И ШИРИНА ШАБЛОНА	*/
+		++i;
+	}
+	new->template = ft_strdup(templates);
+	ISMALLOC_LL(new->template);
+	new->next = NULL;
+	return (new);
 }
 
 int		main()
 {
-	t_ttr	*ptr;
+	t_base_ttr	*ptr;
+	char		*arr;
 
-	ptr = ft_create_ttr();
+	arr = ft_strdup("####|#\n#\n#\n#|##\n##|##.\n.##");
+	ptr = ft_template_ll(arr);
+	printf("%s\n", ptr->template);
+
+
+	return (0);
 }
--- fillit.h
+++ fillit.h
@@ -6,14 +6,33 @@
 /*   By: agottlie <agottlie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 16:12:46 by agottlie          #+#    #+#             */
-/*   Updated: 2018/12/19 16:21:46 by agottlie         ###   ########.fr       */
+/*   Updated: 2018/12/19 17:53:48 by agottlie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-# define ISMALLOC(s) if ((s) == NULL) return (-1)
+# define ISMALLOC_I(s) if ((s) == NULL) return (-1)
+# define ISMALLOC_LL(s) if ((s) == NULL) return (NULL)
+
+typedef struct			s_base_ttr
+{
+	unsigned char		height;
+	unsigned char		width;
+	char				*template;
+	unsigned char		quantity;
+	struct s_base_ttr	*next;
+}						t_base_ttr;
+
+typedef struct		s_ttr
+{
+	unsigned char	height;
+	unsigned char	width;
+	char			*template;
+	unsigned char	letter;
+	struct s_ttr	*next;
+}					t_ttr;
 
 # include <sys/types.h>
 # include <fcntl.h>
